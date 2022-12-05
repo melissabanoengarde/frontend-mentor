554ec0ceec3536f4f37c4dc5501f9dc076ba5ee7
melissa banoen garde <m.banoengarde@gmail.com>
2022-12-05 01:00:18 -0500

    Add backdrop for when navbar is open

--- news-homepage-main/js/hamburger-menu.js
+++ news-homepage-main/js/hamburger-menu.js
@@ -2,11 +2,18 @@ const menu = document.querySelector('.navbar__hamburger');
 const menuIcon = document.querySelector('.navbar__menu-icon');
 const closeIcon = document.querySelector('.close-icon');
 const nav = document.querySelector('.navbar__nav');
+const backdrop = document.querySelector('.navbar__backdrop');
 
 menuIcon.addEventListener('click', () => {
-  nav.classList.add('show');
+  nav.classList.add('navbar-show');
+  // backdrop.classList.add('backdrop-show');
+  backdrop.style.animationName = "fadeIn";
+  backdrop.style.animationDuration = "0.5s";
+  backdrop.style.display = "block";
 })
 
 closeIcon.addEventListener('click', () => {
-  nav.classList.remove('show');
+  nav.classList.remove('navbar-show');
+  backdrop.classList.remove('backdrop-show');
+  backdrop.style.display = "none";
 })
