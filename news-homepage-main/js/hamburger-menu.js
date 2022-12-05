2c072289799061067599cdeecf4b89f2b5463377
melissa banoen garde <m.banoengarde@gmail.com>
2022-12-05 00:18:18 -0500

    Set mobile navigation event listener

--- news-homepage-main/js/hamburger-menu.js
+++ news-homepage-main/js/hamburger-menu.js
@@ -1,3 +1,12 @@
 const menu = document.querySelector('.navbar__hamburger');
-const menuIcon = document.querySelector('.navbar__menu');
-const closeIcon = document.querySelector('.navbar__close');
+const menuIcon = document.querySelector('.navbar__menu-icon');
+const closeIcon = document.querySelector('.close-icon');
+const nav = document.querySelector('.navbar__nav');
+
+menuIcon.addEventListener('click', () => {
+  nav.classList.add('show');
+})
+
+closeIcon.addEventListener('click', () => {
+  nav.classList.remove('show');
+})
